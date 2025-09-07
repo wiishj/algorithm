aaceaacef4986291498153abb43fc1f5d330e1e3
기미진 <79927765+wiishj@users.noreply.github.com>
2025-09-07 19:20:35 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 3.95 ms, Memory: 24.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -1,29 +1,38 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <numeric>
+#include <iostream>
+#include <climits>
+
 using namespace std;
 
 int solution(vector<int> queue1, vector<int> queue2) {
-    int answer = 0;
-    long long sum1 = accumulate(queue1.begin(), queue1.end(), 0ll);
-    long long sum2 = accumulate(queue2.begin(), queue2.end(), 0ll);
-    int idx1 = 0, idx2 = 0;
-    int MAX = queue1.size()*2 + queue2.size()*2;
-    while (sum1 != sum2) {
-        answer++;
-        if (answer > MAX)
-            return -1;
-        if (sum1 < sum2) {
-            sum1 += queue2[idx2];
-            queue1.push_back(queue2[idx2]);
-            sum2 -= queue2[idx2++];
-        }
-        else {
-            sum2 += queue1[idx1];
-            queue2.push_back(queue1[idx1]);
-            sum1 -= queue1[idx1++];
+    int len1=queue1.size(), len2=queue2.size();
+    vector<int> q;
+    long long target=0;
+    long long sum1=0, sum2=0;
+    for(int i:queue1){
+        sum1+=i;
+    }
+    for(int i:queue2){
+       sum2+=i;
+    }
+ 
+    target/=2;
+    int cnt=0, idx1=0, idx2=0;
+    while(sum1!=sum2 && cnt<=len1*2+len2*2){
+        //cout<<idx1<<' '<<idx2<<'\n';
+        cnt++;
+        if(sum1<sum2){
+            sum2-=queue2[idx2];
+            sum1+=queue2[idx2];
+            queue1.push_back(queue2[idx2++]);
+        }else{ //sum1>sum2
+            sum1-=queue1[idx1];
+            sum2+=queue1[idx1];
+            queue2.push_back(queue1[idx1++]);
         }
     }
-    return answer;
+    if(sum1==sum2) return cnt;
+  
+    return -1;
 }
