4ed57c8f8dce5e7fe00a0dac45b781402006537c
기미진 <79927765+wiishj@users.noreply.github.com>
2025-09-24 11:02:36 +0900

    [level 2] Title: 순위 검색, Time: 263.90 ms, Memory: 47.4 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
@@ -1,43 +1,48 @@
 #include <string>
 #include <vector>
+#include <sstream>
 #include <unordered_map>
 #include <algorithm>
-#include <sstream>
 
 using namespace std;
-
-const string ALL = "-";
 unordered_map<string, vector<int>> map;
 
-void insert(string* key, int mask, int point) {
-    string s = "";
-    for (int i = 0; i < 4; i++) {
-        s += (mask & (1 << i)) ? ALL : key[i];
-        map[s].push_back(point);
+void insert(int mask, string key[4], int score){
+    /*
+    mask 몇
+    key의 i번째 정보를 쓸지 말지 결정
+    0이면 정보쓰기 1이면 -
+    2*2*2*2 가지의 경우의 수
+    */
+    string s="";
+    for(int i=0; i<4; i++){
+        s+=(mask & (1<<i))?string("-"):key[i];
     }
+    map[s].push_back(score);
 }
-
 vector<int> solution(vector<string> info, vector<string> query) {
     vector<int> answer;
     string key[4], tmp;
-    int point;
-
-    for (auto& inf : info) {
-        istringstream iss(inf);
-        iss >> key[0] >> key[1] >> key[2] >> key[3] >> point;
-        for (int i = 0; i < 16; i++) insert(key, i, point);
+    int score;
+    
+    for(auto& in: info){
+        istringstream iss(in);
+        iss>>key[0]>>key[1]>>key[2]>>key[3]>>score;
+        for(int i=0; i<16; i++) insert(i, key, score);
     }
-
-    for (auto& m : map) sort(m.second.begin(), m.second.end());
-
-    for (auto& que : query) {
-        istringstream iss(que);
-        iss >> key[0] >> tmp >> key[1] >> tmp >> key[2] >> tmp >> key[3] >>
-            point;
-        string s = key[0] + key[1] + key[2] + key[3];
-        vector<int>& v = map[s];
-        answer.push_back(v.end() - lower_bound(v.begin(), v.end(), point));
+    for(auto& m:map) sort(m.second.begin(), m.second.end());//ascending
+    for(auto& q:query){
+        int ans=0;
+        string s;
+        istringstream iss(q);
+        iss>>key[0]>>tmp>>key[1]>>tmp>>key[2]>>tmp>>key[3]>>score;
+        for(int i=0; i<4; i++) s+=key[i];
+        
+        ans+=(map[s].end()-lower_bound(map[s].begin(), map[s].end(), score));
+        answer.push_back(ans);
     }
-
+    
+    
+    
     return answer;
 }
