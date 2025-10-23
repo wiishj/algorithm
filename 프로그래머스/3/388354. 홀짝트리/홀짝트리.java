2157a6a78b34986323aa392ad7dd8d282ca4b6b1
기미진 <79927765+wiishj@users.noreply.github.com>
2025-10-23 15:42:59 +0900

    [level 3] Title: 홀짝트리, Time: 622.78 ms, Memory: 275 MB -BaekjoonHub

--- 프로그래머스/3/388354. 홀짝트리/홀짝트리.java
+++ 프로그래머스/3/388354. 홀짝트리/홀짝트리.java
@@ -1,76 +1,52 @@
 import java.util.*;
-class Visiter{
-    private int oddNode; // 홀수 -> 역홀
-    private int evenNode; // 짝수 -> 역짝
-    private int inverseOddNode; //역홀 -> 홀수
-    private int inverseEvenNode; // 역짝 -> 짝수
+class Tree{
+    private int cntOdd;
+    private int cntEven;
+    private int cntReverseOdd;
+    private int cntReverseEven;
     
-    public void setNode(int root, int num){
-        int a = root%2;
-        int b = num%2;
-        if(a==0 && b==0){
-            evenNode++;
-        }
-        else if(a!=0 && b!=0){
-            oddNode++;
-        }
-        else if(a!=0 && b==0){
-            inverseOddNode++;
-        }
-        else inverseEvenNode++;
+    public void setNode(int root, int child){
+        if(root%2==1 && child%2==1) cntOdd++;
+        else if(root%2==1 && child%2==0) cntReverseOdd++;
+        else if(root%2==0 && child%2==1) cntReverseEven++;
+        else if(root%2==0 && child%2==0) cntEven++;
     }
-    
-    public int getOddEvenTreeCnt(){
-        if((oddNode==1 && evenNode==0) || (oddNode==0 && evenNode==1))
-            return 1;
-        return 0;
+    public boolean getTree(){
+        if((cntOdd==1 && cntEven==0) || (cntEven==1 && cntOdd==0)) return true;
+        return false;
     }
-    
-    public int getInverseTreeCnt(){
-        if((inverseOddNode==1 && inverseEvenNode==0) || 
-           (inverseOddNode==0 && inverseEvenNode==1))
-            return 1;
-        return 0;
+    public boolean getReverseTree(){
+        if((cntReverseOdd==1 && cntReverseEven==0) || (cntReverseEven==1 && cntReverseOdd==0)) return true;
+        return false;
     }
 }
-
 class Solution {
-    private int[] answer = new int[2];
     private Set<Integer> visited = new HashSet();
-    private Map<Integer, List<Integer>> tree = new HashMap();
-    
+    private Map<Integer, List<Integer>> adj = new HashMap();
+    private int[] answer = new int[2];
     public int[] solution(int[] nodes, int[][] edges) {
-        
-        for(int node : nodes){
-            tree.put(node, new ArrayList());
+        for(int node:nodes) adj.put(node, new ArrayList<Integer>());
+        for(int[] edge:edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
         }
         
-        for(int[] edge : edges){
-            tree.get(edge[0]).add(edge[1]);
-            tree.get(edge[1]).add(edge[0]);
-        }
-        
-        for(int node : nodes){
+        for(int node:nodes){
             if(visited.contains(node)) continue;
             visited.add(node);
-            Visiter visiter = new Visiter();
-            dfs(node, visiter);
-            answer[0] += visiter.getOddEvenTreeCnt();
-            answer[1] += visiter.getInverseTreeCnt();
+            Tree tree = new Tree();
+            dfs(node, tree);
+            if(tree.getTree()) answer[0]++;
+            if(tree.getReverseTree()) answer[1]++;
         }
-        
         return answer;
     }
-    
-    private void dfs(int now, Visiter visiter){
-        int cnt = 0;
-        for(int next : tree.get(now)){
-            cnt++;
-            if(visited.contains(next)) continue;
-            visited.add(next);
-            dfs(next, visiter);
+    private void dfs(int node, Tree tree){
+        tree.setNode(node, adj.get(node).size());
+        for(int child:adj.get(node)){
+            if(visited.contains(child)) continue;
+            visited.add(child);
+            dfs(child, tree);
         }
-        
-        visiter.setNode(now, cnt);
     }
 }
